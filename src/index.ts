ff1fea7ba1b897a4e957462fc812242a4a138124
Umang Galaiya <umanghome@gmail.com>
2024-04-13 17:35:13 +0530

    Expose more methods

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+import { calculateCumulativeTax } from './lib/cumulative.js';
 import { calculateIncomeTax } from './lib/income-tax.js';
+import { calculateSurcharge } from './lib/surcharge.js';
 import { Regime } from './types/regime.js';
 import { FinancialYearStartIn } from './types/years.js';
 
 /**
+ * Get income tax without surcharge and cess.
  *
  * @param {number} year Year in which financial year starts. Eg. "2024"
  * @param age Age of the taxpayer. Eg. 30
@@ -23,4 +26,48 @@ function INCOME_TAX(
   taxableAmount: number,
 ) {
   return calculateIncomeTax(year, age, regime, taxableAmount);
+}
+
+/**
+ * Get surcharge based on taxable income.
+ *
+ * @param {number} year Year in which financial year starts. Eg. "2024"
+ * @param age Age of the taxpayer. Eg. 30
+ * @param regime Regime, "old" or "new"
+ * @param taxableAmount Taxable amount
+ * @returns {number}
+ *
+ * @customfunction
+ *
+ */
+// @ts-expect-error This function is unused in this file and it cannot be exported as `exports.SURCHARGE` will throw an error in Apps Script
+function SURCHARGE(
+  year: FinancialYearStartIn,
+  age: number,
+  regime: Regime,
+  taxableAmount: number,
+) {
+  return calculateSurcharge(year, age, regime, taxableAmount);
+}
+
+/**
+ * Get cumulative income tax — includes surcharge and cess
+ *
+ * @param {number} year Year in which financial year starts. Eg. "2024"
+ * @param age Age of the taxpayer. Eg. 30
+ * @param regime Regime, "old" or "new"
+ * @param taxableAmount Taxable amount
+ * @returns {number}
+ *
+ * @customfunction
+ *
+ */
+// @ts-expect-error This function is unused in this file and it cannot be exported as `exports.CUMULATIVE_INCOME_TAX` will throw an error in Apps Script
+function CUMULATIVE_INCOME_TAX(
+  year: FinancialYearStartIn,
+  age: number,
+  regime: Regime,
+  taxableAmount: number,
+) {
+  return calculateCumulativeTax(year, age, regime, taxableAmount);
 }
