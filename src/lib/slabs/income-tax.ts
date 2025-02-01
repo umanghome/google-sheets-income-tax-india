47966204f41eeb6130982d1fcad6f834753d1cb3
Umang Galaiya <umanghome@gmail.com>
2025-02-01 12:53:40 +0530

    Add 2025 slabs (#1)
    * Add new slabs
    
    * build

--- src/lib/slabs/income-tax.ts
+++ src/lib/slabs/income-tax.ts
@@ -53,12 +53,28 @@ const _INCOME_TAX_SLABS_NEW_REGIME_2023: IncomeTaxSlabs[number][1] = [
   ],
 ];
 
+const _INCOME_TAX_SLABS_NEW_REGIME_2025: IncomeTaxSlabs[number][1] = [
+  [
+    Infinity,
+    [
+      [lakhs(4), 0],
+      [lakhs(8), 5],
+      [lakhs(12), 10],
+      [lakhs(16), 15],
+      [lakhs(20), 20],
+      [lakhs(24), 25],
+      [Infinity, 30],
+    ],
+  ],
+];
+
 const _INCOME_TAX_SLABS_OLD_REGIME: IncomeTaxSlabs = [
   [2023, _INCOME_TAX_SLABS_OLD_REGIME_2023],
 ] as const;
 
 const _INCOME_TAX_SLABS_NEW_REGIME: IncomeTaxSlabs = [
   [2023, _INCOME_TAX_SLABS_NEW_REGIME_2023],
+  [2025, _INCOME_TAX_SLABS_NEW_REGIME_2025],
 ];
 
 export const INCOME_TAX_SLABS_BY_REGIME: Record<Regime, IncomeTaxSlabs> = {
